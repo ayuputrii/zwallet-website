b7fadb277f1f4b90269e238e843a31dc8d2ef64a
ayuputrii <ayuarmadani.pm@gmail.com>
2020-12-10 19:34:08 +0700

    utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,3 @@
-export const imageURI = "http://localhost:8000/images/";
-export const URI = "http://localhost:8000/api/v1";
-export const URL_ADM = "http://localhost:8000/admin/api/v1";
+export const imageURI = "https://zwallet-website.herokuapp.com/images/";
+export const URI = "https://zwallet-website.herokuapp.com/api/v1";
+export const URL_ADM = "https://zwallet-website.herokuapp.com/admin/api/v1";
